5f9ee34471199231214c2e5a2141830d2a509e60
rakeshsenapathi <rakeshsenapathi@gmail.com>
2016-07-06 00:42:39 +0530

    Final Commit

--- app/src/main/java/com/senapathi/feocloud/MainActivity.java
+++ app/src/main/java/com/senapathi/feocloud/MainActivity.java
@@ -1,5 +1,6 @@
 package com.senapathi.feocloud;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -42,44 +44,105 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.listView);
         mAdapter = new MyAdapter(this, mListItems);
         listView.setAdapter(mAdapter);
-        //
+        // Typecasting for bottom toolbar
         mSelectedTrackTitle = (TextView) findViewById(R.id.selected_track_title);
         mSelectedTrackImage = (ImageView) findViewById(R.id.selected_track_image);
         mToolbar = (CardView) findViewById(R.id.toolbar);
+        mPlayerControl = (ImageView) findViewById(R.id.player_control);
+        mPlayerControl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                togglePlayPause();
+            }
+        });
+        //Media player function
+        MyMediaPlayer();
         //
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(mToolbar != null)
-                mToolbar.setVisibility(View.VISIBLE);
+                if (mToolbar != null)
+                    mToolbar.setVisibility(View.VISIBLE);
                 Track track = mListItems.get(position);
                 mSelectedTrackTitle.setText(track.getTitle());
                 if (track.getArtworkURL() != null)
                     Picasso.with(MainActivity.this).load(track.getArtworkURL()).into(mSelectedTrackImage);
                 else
                     Picasso.with(MainActivity.this).load(Utils.dummyImage).into(mSelectedTrackImage);
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.stop();
+                    mMediaPlayer.reset();
+                }
 
-
+                try {
+                    mMediaPlayer.setDataSource(track.getStreamURL() + "?client_id=" + Config.CLIENT_ID);
+                    mMediaPlayer.prepareAsync();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
-
+        //
         //
         // Making service call to get recent tracks
         SCService scService = SoundCloud.getService();
+        Utils.showdialog(this);
         scService.getRecentTracks(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()), new Callback<List<Track>>() {
                     @Override
                     public void success(List<Track> tracks, Response response) {
-                        Log.d(TAG, "First track title :" + tracks.get(0).getTitle());
-                        loadTracks(tracks);
+                        if (!tracks.isEmpty()) {
+                            Utils.dismissdialog(MainActivity.this);
+                            Log.d(TAG, "First track title :" + tracks.get(0).getTitle());
+                            loadTracks(tracks);
+                        }
+                        //
+                        // else{
+                        // Utils.dismissdialog(MainActivity.this);}
+
                     }
 
                     @Override
                     public void failure(RetrofitError error) {
+                        Utils.dismissdialog(MainActivity.this);
                         Log.d(TAG, "Error occured");
+
                     }
                 }
         );
+        //
+
     }
+
+    private void MyMediaPlayer() {
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                togglePlayPause();
+            }
+        });
+        //
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mPlayerControl.setImageResource(R.drawable.playbtn);
+            }
+        });
+
+    }
+
+    private void togglePlayPause() {
+
+        if (mMediaPlayer.isPlaying()) {
+            mMediaPlayer.pause();
+            mPlayerControl.setImageResource(R.drawable.playbtn);
+        } else {
+            mMediaPlayer.start();
+            mPlayerControl.setImageResource(R.drawable.pausebtn);
+        }
+    }
+
 
     private void loadTracks(List<Track> tracks) {
 
@@ -88,5 +151,17 @@ public class MainActivity extends AppCompatActivity {
         mAdapter.notifyDataSetChanged();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mMediaPlayer != null) {
+            if (mMediaPlayer.isPlaying()) {
+                mMediaPlayer.stop();
+            }
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
 
 }
--- app/src/main/java/com/senapathi/feocloud/Utils.java
+++ app/src/main/java/com/senapathi/feocloud/Utils.java
@@ -1,9 +1,30 @@
 package com.senapathi.feocloud;
 
+import android.content.Context;
+
+import com.kaopiz.kprogresshud.KProgressHUD;
+
 /**
  * Created by Senapathi on 05-07-2016.
  */
 public class Utils {
 
     public static final String dummyImage = "https://s-media-cache-ak0.pinimg.com/236x/8d/e4/20/8de42050e671b93b1d6bad2f2764ba89.jpg";
+
+    public static KProgressHUD kProgressHUD;
+    public static void showdialog(Context context){
+        if(kProgressHUD == null){
+        KProgressHUD.create(context)
+                .setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
+                .setLabel("Loading...")
+        .setCancellable(true)
+                .setAnimationSpeed(2)
+                .setDimAmount(0.5f)
+                .show();}
+    }
+
+    public static void dismissdialog(Context context)
+    {
+        kProgressHUD.dismiss();
+    }
 }
